0fd1066c9d23d13cba18a647b4ab95df036b20d9
Piotr Głębocki <piotr.glebocki@stepstone.com>
2017-04-12 14:45:30 +0200

    [material-app-rating] refactored rating bar

--- app-rating/src/main/java/com/stepstone/apprating/ratingbar/CustomRatingBar.java
+++ app-rating/src/main/java/com/stepstone/apprating/ratingbar/CustomRatingBar.java
@@ -17,111 +17,74 @@ limitations under the License.
 package com.stepstone.apprating.ratingbar;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Shader;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ClipDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RoundRectShape;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Build;
-import android.support.annotation.Nullable;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v7.widget.AppCompatDrawableManager;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.Gravity;
-import android.widget.RatingBar;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.stepstone.apprating.AppRatingDialogView;
 import com.stepstone.apprating.R;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 /**
  * This class is a custom rating bar. It handles displaying of
  * stars, tinting etc.
  */
-public class CustomRatingBar extends RatingBar {
+public class CustomRatingBar extends LinearLayout {
 
-    @Nullable
-    private Bitmap sampleTile;
+    private LinearLayout container;
+
+    private int numStars;
+    private int max;
+    private float rating;
+    private boolean isIndicator;
+    private float stepSize;
+    private AppRatingDialogView onRatingBarChangeListener;
 
     public CustomRatingBar(final Context context, final AttributeSet attrs) {
         super(context, attrs);
-        setProgressDrawable(createProgressDrawable());
+        LayoutInflater.from(context).inflate(R.layout.component_custom_rating_bar, this);
+        container = (LinearLayout) findViewById(R.id.rating_bar_container);
+
+        drawStars();
     }
 
-    @Override
-    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        if (sampleTile != null) {
-            final int width = sampleTile.getWidth() * getNumStars();
-            setMeasuredDimension(resolveSizeAndState(width, widthMeasureSpec, 0),
-                    getMeasuredHeight());
-        }
+    private void drawStars() {
+        StarButton starButton = new StarButton(getContext());
+        starButton.setImageResource(R.drawable.ic_star_full_48dp);
+        starButton.setLayoutParams(new LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+        container.addView(starButton);
     }
 
-    protected LayerDrawable createProgressDrawable() {
-        final Drawable backgroundDrawable = createBackgroundDrawableShape();
-        LayerDrawable layerDrawable = new LayerDrawable(new Drawable[]{
-                backgroundDrawable,
-                backgroundDrawable,
-                createProgressDrawableShape()
-        });
-        layerDrawable.setId(0, android.R.id.background);
-        layerDrawable.setId(1, android.R.id.secondaryProgress);
-        layerDrawable.setId(2, android.R.id.progress);
-        return layerDrawable;
+    public float getRating() {
+        return rating;
     }
 
-    protected Drawable createBackgroundDrawableShape() {
-        Drawable drawable = AppCompatDrawableManager.get().getDrawable(getContext(), R.drawable.ic_star_empty_48dp);
-        DrawableCompat.setTint(drawable, getThemeAccentColor(getContext()));
-
-        final Bitmap tileBitmap = drawableToBitmap(drawable);
-        if (sampleTile == null) {
-            sampleTile = tileBitmap;
-        }
-        final ShapeDrawable shapeDrawable = new ShapeDrawable(getDrawableShape());
-        final BitmapShader bitmapShader = new BitmapShader(tileBitmap, Shader.TileMode.REPEAT, Shader.TileMode.CLAMP);
-        shapeDrawable.getPaint().setShader(bitmapShader);
-        return shapeDrawable;
+    public void setNumStars(int numStars) {
+        this.numStars = numStars;
     }
 
-    protected Drawable createProgressDrawableShape() {
-        Drawable drawable = AppCompatDrawableManager.get().getDrawable(getContext(), R.drawable.ic_star_full_48dp);
-        DrawableCompat.setTint(drawable, getThemeAccentColor(getContext()));
-        final Bitmap tileBitmap = drawableToBitmap(drawable);
-        final ShapeDrawable shapeDrawable = new ShapeDrawable(getDrawableShape());
-        final BitmapShader bitmapShader = new BitmapShader(tileBitmap, Shader.TileMode.REPEAT, Shader.TileMode.CLAMP);
-        shapeDrawable.getPaint().setShader(bitmapShader);
-        return new ClipDrawable(shapeDrawable, Gravity.LEFT, ClipDrawable.HORIZONTAL);
+    public void setMax(int max) {
+        this.max = max;
     }
 
-    Shape getDrawableShape() {
-        final float[] roundedCorners = new float[]{5, 5, 5, 5, 5, 5, 5, 5};
-        return new RoundRectShape(roundedCorners, null, null);
+    public void setRating(float rating) {
+        this.rating = rating;
     }
 
-    public static Bitmap drawableToBitmap(Drawable drawable) {
-        if (drawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) drawable).getBitmap();
-        }
+    public void setIsIndicator(boolean isIndicator) {
+        this.isIndicator = isIndicator;
+    }
 
-        int width = drawable.getIntrinsicWidth();
-        width = width > 0 ? width : 1;
-        int height = drawable.getIntrinsicHeight();
-        height = height > 0 ? height : 1;
+    public void setStepSize(float stepSize) {
+        this.stepSize = stepSize;
+    }
 
-        final Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        final Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-
-        return bitmap;
+    public void setOnRatingBarChangeListener(AppRatingDialogView onRatingBarChangeListener) {
+        this.onRatingBarChangeListener = onRatingBarChangeListener;
     }
 
     private static int getThemeAccentColor(Context context) {
---
+++ app-rating/src/main/java/com/stepstone/apprating/ratingbar/StarButton.java
@@ -0,0 +1,21 @@
+package com.stepstone.apprating.ratingbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * @author Piotr Głębocki (Piotr.Glebocki@stepstone.com) on 12/04/2017.
+ */
+public class StarButton extends ImageView {
+
+    public StarButton(Context context) {
+        super(context);
+    }
+
+    public StarButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // TODO define methods, animations
+}
